ab2b199ad18ec848ba9570d89fb41d9e59f74dbf
Niky22 <109652380+Niky22@users.noreply.github.com>
2022-07-20 23:08:01 +0300

    Add files via upload

---
+++ Init.py
@@ -0,0 +1,47 @@
+import asyncio
+import os
+import logging
+from aiogram.utils.executor import start_webhook
+import aiogram.types
+from aiogram import Bot, Dispatcher, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.contrib.middlewares.logging import LoggingMiddleware
+BOT_TOKEN = "5468584401:AAHFgWlNuZEalDjX3pePvvbh6JySNiembIs"
+loop = asyncio.get_event_loop()
+storage = MemoryStorage()
+bot = Bot(BOT_TOKEN, parse_mode="HTML")
+dp = Dispatcher(bot, loop=loop, storage=storage)
+dp.middleware.setup(LoggingMiddleware())
+# webhook settings
+WEBHOOK_HOST = "https://f1cards.herokuapp.com/"
+WEBHOOK_PATH = f'/webhook/{BOT_TOKEN}'
+WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
+
+# webserver settings
+WEBAPP_HOST = '0.0.0.0'
+WEBAPP_PORT = os.getenv('PORT', default=8000)
+
+
+async def on_startup(dispatcher):
+    await bot.set_webhook(WEBHOOK_URL, drop_pending_updates=True)
+
+
+async def on_shutdown(dispatcher):
+    await bot.delete_webhook()
+
+
+if __name__ == "__main__":
+    from aiogram import executor
+    from main import dp
+    from admin import dp
+    from cards_inf import dp
+    logging.basicConfig(level=logging.INFO)
+    start_webhook(
+        dispatcher=dp,
+        webhook_path=WEBHOOK_PATH,
+        skip_updates=True,
+        on_startup=on_startup,
+        on_shutdown=on_shutdown,
+        host=WEBAPP_HOST,
+        port=WEBAPP_PORT,
+    )
